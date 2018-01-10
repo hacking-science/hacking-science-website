dbbe72f3386bf4c288e7abab3466550fa0bb6bf1
MaxAwan <max.awan@digital.justice.gov.uk>
2018-01-10 21:12:49 +0000

    add app to settings

--- hackscience/settings.py
+++ hackscience/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -67,6 +68,10 @@ TEMPLATES = [
         },
     },
 ]
+
+TEMPLATE_DIRS = (
+    "",
+)
 
 WSGI_APPLICATION = 'hackscience.wsgi.application'
 
