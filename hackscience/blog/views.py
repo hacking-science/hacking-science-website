677d33450759ade01f7de5d7d4b45a09a644680c
MaxAwan <max.awan@digital.justice.gov.uk>
2018-01-10 20:44:00 +0000

    Initial work

--- hackscience/blog/views.py
+++ hackscience/blog/views.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+
+    context = {}
+    return render(request, "index.html", context)
