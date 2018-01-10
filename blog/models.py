e66a97265c6f2654ae7cfcb5eba9beee97c150a5
MaxAwan <max.awan@digital.justice.gov.uk>
2018-01-10 21:49:38 +0000

    Working index and first migration

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,32 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class AbstractBaseClass(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        abstract = True
+
+class Post(AbstractBaseClass):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+
+class Tag(AbstractBaseClass):
+    title = models.CharField(max_length=100)
+
+
+class Location(AbstractBaseClass):
+    title = models.CharField(max_length=100)
+
+
+class PostTag(AbstractBaseClass):
+    post = models.ForeignKey("Post")
+    tag = models.ForeignKey("Tag")
+
+
+class PostLocation(AbstractBaseClass):
+    post = models.ForeignKey("Post")
+    location = models.ForeignKey("Location")
+
+class Comment(AbstractBaseClass):
+    post = models.ForeignKey("Post")
--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import url
 from blog import views
 
 
-urlpatterns = [                       url(r'^$', views.index, name='index'),
-
-
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
                        ]
