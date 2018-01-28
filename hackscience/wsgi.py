14b44cc400ae863f7b9b88af6d933821a2043727
MaxAwan <max.awan@digital.justice.gov.uk>
2018-01-28 17:52:13 +0000

    New wsgi

--- hackscience/wsgi.py
+++ hackscience/wsgi.py
@@ -12,6 +12,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+path = '/home/ubuntu/hacking-science-website'
+if path not in sys.path:
+    sys.path.append(path)
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackscience.settings")
 
 application = get_wsgi_application()
