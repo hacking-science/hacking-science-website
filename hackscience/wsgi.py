c8f3cd5377daf7ea62ab485e524999e83062399a
MaxAwan <max.awan@digital.justice.gov.uk>
2018-01-28 18:00:35 +0000

    Fix wsgi

--- hackscience/wsgi.py
+++ hackscience/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
