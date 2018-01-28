c0a722c795388f6f04c4ac77660d7beebf48fc8e
Alec Doran-Twyford <alec@alectronic.co>
2018-01-28 17:40:39 +0000

    fixed index and read admin

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -1,22 +1,18 @@
 # create_admin.py
 import os
+from django.contrib.auth.models import User
+
 
 def run():
-    if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
-        from django.contrib.auth.models import User
-
-        user = User.objects.create_user(os.environ['ADMIN_USER'], password=os.environ['ADMIN_PASSWORD'])
+    try:
+        if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
+            user = User.objects.create_user(os.environ['ADMIN_USER'], password=os.environ['ADMIN_PASSWORD'])
+        else:
+            user = User.objects.create_user("admin", password="password")
         user.is_superuser = True
         user.is_staff = True
         user.save()
         print("admin created")
-    else:
-        from django.contrib.auth.models import User
-
-        user = User.objects.create_user("admin", password="password")
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
-        print("admin created")
-
-    print("admin creation failed")
+    except:
+        print("Failed to create user")
+        print("Super User already exists")
