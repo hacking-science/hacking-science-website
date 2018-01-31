f4402b620e3190ced7b6cb4f1f6b217de3d149f9
Harjeet Kalsi <harjeet.kalsi@solirius.com>
2018-01-31 20:10:26 +0000

    format

--- blog/views.py
+++ blog/views.py
@@ -110,6 +110,7 @@ def post(request, post_id):
 
     context = {"post": post}
     return render(request, "post/single.html", context)
+
 def about(request):
     """Generate About Page"""
     context={}
